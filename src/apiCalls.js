50f35f664a2482826120b2d2d182fa3dd732226a
Matthew Lane <themplane@gmail.com>
2020-11-10 08:38:59 -0800

    Update api calls.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,34 @@
-export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+const api = {
+  url: 'http://localhost:3001/api/v1/urls/',
+
+  getUrls: () => {
+    return fetch(api.url)
+    .then(response => {
+      if (response.ok) return response.json();
+      return 'error'
+    })
+  },
+
+  postUrl: (long_url, title) => {
+    return fetch(api.url, {
+      body: JSON.stringify( {long_url, title}),
+      method: 'POST',
+      header: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if (response.ok) return response.json();
+      return 'error'
+    })
+  },
+
+  deleteUrl: (id) => {
+    return fetch(api.url + id, {
+      method: 'DELETE',
+    })
+    .then(response => response.ok)
+  }
 }
+
+export default api;
